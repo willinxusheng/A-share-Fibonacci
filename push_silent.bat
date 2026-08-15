beaaae09d39a2723a4e32d2c21c8170aded0e9de
willin <willinxusheng@163.com>
2026-08-15 20:02:35 +0800

    fix: set GIT_TERMINAL_PROMPT=0 to avoid SSH prompt hang in non-interactive cmd

--- push_silent.bat
+++ push_silent.bat
@@ -1,4 +1,5 @@
 @echo off
+set "GIT_TERMINAL_PROMPT=0"
 cd /d "C:\Users\Administrator\WorkBuddy\2026-08-04-23-16-18\a-share-fib-wave"
 C:\Users\Administrator\.workbuddy\binaries\PortableGit\versions\1.2.0\cmd\git.exe add -A
 C:\Users\Administrator\.workbuddy\binaries\PortableGit\versions\1.2.0\cmd\git.exe commit -m "chore: auto-update" >nul 2>&1
